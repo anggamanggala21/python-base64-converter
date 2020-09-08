84cd038d1455ed31969f125a51232773f2d7a59e
anggamanggala21 <anggamanggala21@gmail.com>
2020-09-08 12:18:57 +0800

    First commit

---
+++ decode.py
@@ -0,0 +1,25 @@
+from base64 import b64decode
+import random
+import string
+import pathlib
+
+file_name = input("Enter file name : ")
+try:
+
+	# READ & CONVERT
+	f = open(file_name, "r")	
+	base64 = b64decode(f.read())	
+	f.close()
+
+	# SAVE
+	ranstr = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+	new_file_name = "dec_" + ranstr + pathlib.Path(file_name).suffix
+	fw = open(new_file_name, "x")
+	fw.write(str(base64.decode()))
+	fw.close()
+	
+	print("Decode file saved in file ", new_file_name)
+	print("Success !")
+
+except Exception:
+	print("File not found !")
---
+++ encode.py
@@ -0,0 +1,26 @@
+from base64 import b64encode
+import random
+import string
+import pathlib
+
+file_name = input("Enter file name : ")
+try:
+
+	# READ & CONVERT
+	f = open(file_name, "r")
+	ascii = f.read().encode('ascii')
+	base64 = b64encode(ascii)	
+	f.close()
+
+	# SAVE
+	ranstr = ''.join(random.choice(string.ascii_lowercase) for i in range(5))
+	new_file_name = "enc_" + ranstr + pathlib.Path(file_name).suffix
+	fw = open(new_file_name, "x")
+	fw.write(str(base64.decode()))
+	fw.close()
+
+	print("Encode file saved in file ", new_file_name)
+	print("Success !")
+
+except Exception:
+	print("File not found !")
